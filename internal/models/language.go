2fd83f1a200ec96c6a66e3a122ba681d83f7c932
Konstantin Kolesnikov <kosstanley@gmail.com>
2023-07-16 21:24:41 +0200

    Remove constraint.

--- internal/models/language.go
+++ internal/models/language.go
@@ -8,6 +8,6 @@ type Language struct {
 	gorm.Model
 	Code        string `gorm:"type:varchar(3);unique;not null" json:"code"`
 	Text        string `gorm:"type:varchar(255);not null" json:"text"`
-	EnglishText string `gorm:"type:varchar(255);not null" json:"english_text"`
+	EnglishText string `gorm:"type:varchar(255)" json:"english_text"`
 	Emoji       string `gorm:"type:varchar(10);not null" json:"emoji"`
 }
